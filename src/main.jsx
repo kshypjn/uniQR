91f133855bcb17ea1b2bced3a04e6c07158ef7f9
Kashyap J <kashyapj@wholesomeAir.local>
2024-08-28 09:52:46 +0530

    estimate

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import QRCodeGenerator from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QRCodeGenerator />
   </StrictMode>,
 )
